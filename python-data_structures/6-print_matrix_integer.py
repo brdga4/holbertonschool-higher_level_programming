7326683a2116ac4a03b145256dda9ab3acce5778
Basem Tashkandi <basem.tashkandi@gmail.com>
2026-06-12 18:36:11 +0300

    Task 6 att3

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -3,5 +3,7 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for i in row:
-            print("{:d}".format(i), end=" ")
+            print("{:d}".format(i), end="")
+            if row[len(row) - 1] != i:
+                print(" ", end="")
         print()
