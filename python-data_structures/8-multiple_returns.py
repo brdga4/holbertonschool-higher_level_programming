cbca1f411d35191fb8a2825e75c22c365410d46f
Basem Tashkandi <basem.tashkandi@gmail.com>
2026-06-12 20:51:39 +0300

    Task 8 att2

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 def multiple_returns(sentence):
-    if sentence is None:
-        sentence[0] = None
-    return (len(sentence), sentence[0])
+    if sentence == "":
+        first_char = "None"
+    else:
+        first_char = sentence[0]
+    return (len(sentence), first_char)
