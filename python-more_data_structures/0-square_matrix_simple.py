e9c4ee065e51df5e5dbfc3d56beccda7cc04909c
Basem Tashkandi <basem.tashkandi@gmail.com>
2026-06-13 01:25:15 +0300

    Task 0 att2

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 
 def square_matrix_simple(matrix=[[]]):
-    return (list(map(lambda row: list(map(lambda index: index ** 2, row)), matrix)))
+    result = list(map(lambda row: list(map(lambda index: index ** 2, row)), matrix))
+    return (result)
