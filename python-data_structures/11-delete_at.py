57f0e81460773266e891aad3a1e910b019b09033
Basem Tashkandi <basem.tashkandi@gmail.com>
2026-06-12 22:28:45 +0300

    Task  11 att2

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -2,7 +2,6 @@
 
 def delete_at(my_list=[], idx=0):
     if len(my_list) < idx < 0:
-        return (my_list)
-    new_list = my_list.copy()
-    del new_list[idx]
-    return (new_list)
+        return
+    del my_list[idx]
+    return (my_list)
