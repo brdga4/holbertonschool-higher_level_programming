5ca3788d85d4d31201607bee03dcb15bc95bcfbd
Basem Tashkandi <basem.tashkandi@gmail.com>
2026-06-13 04:15:29 +0300

    Task 7 att2

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
 
 def update_dictionary(a_dictionary, key, value):
-    if key in a_dictionary:
-        a_dictionary[key] = value
-    else:
-        new_dic = list(a_dictionary)
-        a_dictionary.append((key, value))
-        dict(a_dictionary)
+    a_dictionary[key] = value
+    return (a_dictionary)
