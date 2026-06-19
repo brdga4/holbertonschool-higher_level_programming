658bc45e8bf8711168f2a70543afd3b685824ccc
Basem Tashkandi <basem.tashkandi@gmail.com>
2026-06-19 14:22:57 +0300

    Task 0 att2

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,5 +1,16 @@
 #!/usr/bin/python3
+"""This module defines a simple, empty Square class.
+
+It serves as the foundational step for learning Object-Oriented Programming
+and class definitions in Python.
+"""
 
 
 class Square:
+    """Represents a square shape.
+
+    This class is currently empty and will be expanded in future tasks to
+    include size attributes and area methods.
+    """
+
     pass
