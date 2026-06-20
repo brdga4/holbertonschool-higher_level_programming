aa45ef0e34ce9ea907f09c414726380f9cb5ccd4
Basem Tashkandi <basem.tashkandi@gmail.com>
2026-06-20 22:19:53 +0300

    Task 1 att1

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""
+A class that creates a rectangle
+"""
+
+
+class Rectangle:
+    """
+    A class that creates a rectangle
+    """
+
+    def __init__(self, width=0, height=0):
+        """
+        Initializes the object
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """getter"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """setter"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """getter"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """setter"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
