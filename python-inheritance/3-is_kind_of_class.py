38905c632cf420016a4e7c0fb757e67e648133dd
Basem Tashkandi <basem.tashkandi@gmail.com>
2026-06-27 05:05:07 +0300

    Task 3 att3

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -6,7 +6,7 @@ an instance of a class that inherited, the specified class
 """
 
 
-def is_same_class(obj, a_class):
+def is_kind_of_class(obj, a_class):
     """
     returns True if the object is an instance of, or if the object is
     an instance of a class that inherited, the specified class
