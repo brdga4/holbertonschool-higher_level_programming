849c8485703f84dc58f70da0adff00384c0caf27
Basem Tashkandi <basem.tashkandi@gmail.com>
2026-06-27 05:14:05 +0300

    Task 4 att1

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+returns True if the object is an instance of
+a class that inherited (directly or indirectly) the specified class
+; otherwise False
+"""
+
+
+def inherits_from(obj, a_class):
+    """
+    returns True if the object is an instance of
+    a class that inherited (directly or indirectly) the specified class
+    ; otherwise False
+    """
+
+    return issubclass(obj, a_class)
