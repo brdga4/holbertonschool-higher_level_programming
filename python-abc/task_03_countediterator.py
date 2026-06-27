381dce48da4aad4a89bc4e104c87a7b6894935cd
Basem Tashkandi <basem.tashkandi@gmail.com>
2026-06-27 10:43:53 +0300

    Task 3 att1

---
+++ python-abc/task_03_countediterator.py
@@ -0,0 +1,11 @@
+class CountedIterator:
+    def __init__(self, iterable):
+        self.iterator = iter(iterable)
+        self.counter = 0
+
+    def get_count(self):
+        return self.counter
+
+    def __next__(self):
+        self.counter += 1
+        return next(self.iterator)
