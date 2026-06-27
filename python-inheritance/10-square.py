3e0046b4cd2e0af5207f52259d64c24c1b58cf36
Basem Tashkandi <basem.tashkandi@gmail.com>
2026-06-27 07:00:20 +0300

    Task 10 att2

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python3
+"""
+Square parent class
+"""
 
 Rectangle = __import__("9-rectangle").Rectangle
 
 
 class Square(Rectangle):
+    """
+    Square parent class
+    """
+
     def __init__(self, size):
         self.integer_validator("size", size)
         self.__size = size
