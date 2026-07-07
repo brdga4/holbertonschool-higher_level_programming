0720d6dedd37cb441b0c51fe67e42d11f747b5b0
Basem Tashkandi <basem.tashkandi@gmail.com>
2026-07-07 16:36:03 +0300

    Task 0 att1

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""
+Script
+"""
+
+
+def read_file(filename=""):
+    """
+    Function
+    """
+    with open(filename, "r", encoding="utf-8") as file:
+        file.read()
