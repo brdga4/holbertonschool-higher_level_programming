e316b63de814129c7b5aaa1f7d694477a753efe3
Basem Tashkandi <basem.tashkandi@gmail.com>
2026-07-11 19:53:29 +0300

    Task 7 att1

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Script
+"""
+
+import sys
+import os
+
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+
+args = sys.argv[1:]
+
+file = "add_item.json"
+
+if os.path.exists(file):
+    my_list = load_from_json_file(file)
+else:
+    my_list = []
+
+my_list.append(args)
+save_to_json_file(my_list, file)
