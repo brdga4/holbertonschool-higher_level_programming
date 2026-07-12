9cb0a45a28eb2ea6b959f647c5c0aaa76ad026ac
Basem Tashkandi <basem.tashkandi@gmail.com>
2026-07-12 07:11:50 +0300

    Task 0 att1

---
+++ python-serialization/task_00_basic_serialization.py
@@ -0,0 +1,11 @@
+import json
+
+
+def serialize_and_save_to_file(data, filename):
+    with open(filename, "w", encoding="utf-8") as file:
+        json.dump(data, file)
+
+
+def load_and_deserialize(filename):
+    with open(filename, "r", encoding="utf-8") as file:
+        return json.load(file)
