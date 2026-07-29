26f7f8072f1914551cdf6be250729b50190165b7
Basem Tashkandi <basem.tashkandi@gmail.com>
2026-07-29 13:00:07 +0300

    Task 7 att1

---
+++ SQL_more_queries/7-cities.sql
@@ -0,0 +1,11 @@
+-- Comment
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+
+CREATE TABLE cities (
+  id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
+  state_id INT NOT NULL PRIMARY KEY,
+  name VARCHAR(256) NOT NULL,
+  FOREIGN KEY (state_id) REFERENCES states(id)
+);
+
