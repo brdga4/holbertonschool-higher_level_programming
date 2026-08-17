9215ff98bdc01912b3724ac6fb06ea8735a02808
Basem Tashkandi <basem.tashkandi@gmail.com>
2026-08-17 20:05:17 +0300

    Task 8 att2

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -4,6 +4,6 @@ if (Number.isNaN(size)) {
   console.log('Missing size');
 } else {
   for (let i = 0; i < size; i++) {
-    console.log('#'.repeat(size));
+    console.log('X'.repeat(size));
   }
 }
