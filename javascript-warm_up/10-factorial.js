ef28199ba92da4aa99116ef4b2af6367c085086a
Basem Tashkandi <basem.tashkandi@gmail.com>
2026-08-17 20:41:08 +0300

    Task 10 att1

---
+++ javascript-warm_up/10-factorial.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+function factorial (num) {
+  if (num === 0 || num === 1) {
+    return (1);
+  }
+  return (num * factorial(num - 1));
+}
+
+const num = parseInt(process.argv[2]);
+if (Number.isNaN(num)) {
+  console.log(1);
+} else {
+  console.log(factorial(num));
+}
