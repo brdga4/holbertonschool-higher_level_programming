9031c4c1f6c7fcf492df93e6702f7c88ff166618
Basem Tashkandi <basem.tashkandi@gmail.com>
2026-08-17 20:14:06 +0300

    Task 9 att1

---
+++ javascript-warm_up/9-add.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+function add (a, b) {
+  console.log(a + b);
+}
+
+const num1 = parseInt(process.argv[2]);
+const num2 = parseInt(process.argv[3]);
+
+if (Number.isNaN(num1) || Number.isNaN(num2)) {
+  console.log('NaN');
+} else {
+  add(num1, num2);
+}
