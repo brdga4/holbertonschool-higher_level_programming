873bf3a4b3d5bb2f2c716b0e974f71945aee67e2
Basem Tashkandi <basem.tashkandi@gmail.com>
2026-08-17 08:34:50 +0300

    Task 5 att2

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -3,5 +3,5 @@ const myNum = parseInt(process.argv[2], 10);
 if (Number.isNaN(myNum) || myNum === undefined) {
   console.log('Not a number');
 } else {
-  console.log(myNum);
+  console.log(`My number: ${myNum}`);
 }
