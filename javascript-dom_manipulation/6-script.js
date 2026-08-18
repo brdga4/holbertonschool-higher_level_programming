aad653a4a14318bd2112d21d6e7cf54e377d940f
Basem Tashkandi <basem.tashkandi@gmail.com>
2026-08-18 07:35:45 +0300

    Task 6 att1

---
+++ javascript-dom_manipulation/6-script.js
@@ -0,0 +1,11 @@
+fetch('https://swapi-api.hbtn.io/api/people/5/?format=json')
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    const character = document.querySelector('#character');
+    character.textContent = data.name;
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
